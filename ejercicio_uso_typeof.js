fb42e491c122850bbb3f7ae6460e639a306307c3
gallopelado <juanftp100@gmail.com>
2024-01-16 18:31:18 -0300

    add: se corrigio el uso del typeof

--- ejercicio_uso_typeof.js
+++ ejercicio_uso_typeof.js
@@ -5,8 +5,8 @@ let val1 = undefined;
 let val2 = null;
 let myNum = 1000;
 
-console.log(`El tipo de dato de str1 es ${str1}`);
-console.log(`El tipo de dato de str2 es ${str2}`);
-console.log(`El tipo de dato de val1 es ${val1}`);
-console.log(`El tipo de dato de val2 es ${val2}`);
-console.log(`El tipo de dato de myNum es ${myNum}`);
+console.log(`El tipo de dato de str1 es ${typeof str1}`);
+console.log(`El tipo de dato de str2 es ${typeof str2}`);
+console.log(`El tipo de dato de val1 es ${typeof val1}`);
+console.log(`El tipo de dato de val2 es ${typeof val2}`);
+console.log(`El tipo de dato de myNum es ${typeof myNum}`);
