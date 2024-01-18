589dbfd8e2409e204ccd3dbbaf21e3b8186bb676
gallopeladoprofe <juanftp100.edu@gmail.com>
2024-01-18 19:01:47 -0300

    ejemplo largo de como hacer mensajes para el commit
    *********
    
    ipukuma ajepa?

---
+++ tarea_2.4.js
@@ -0,0 +1,11 @@
+/*
+Crea variables para tres números: a, b, c. Actualiza los valores de esas variables usando los operadores de asignación:
+
+Suma b en a
+Divide a con c
+Reemplaza el valor de c con el módulo de c y b
+Imprimir los tres números en consola
+
+*/
+
+let a = 0, b = 0, c = 0;
