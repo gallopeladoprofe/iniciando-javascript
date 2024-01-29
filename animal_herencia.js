45519ed92a2048fce3a2792cb0da6c1ede629fb1
gallopeladoprofe <juanftp100.edu@gmail.com>
2024-01-29 19:17:31 -0300

    agregando herencia de animal

---
+++ animal_herencia.js
@@ -0,0 +1,34 @@
+class Animal {
+    #patas = 4;
+    constructor(patas) {
+        this.#patas = patas;
+    }
+}
+
+class Mamifero extends Animal {
+    constructor(patas) {
+        // cuando se hace la herencia, es obligatorio esto:
+        super(patas);
+    }
+}
+
+class Pez extends Animal {
+    constructor(patas) {
+        // cuando se hace la herencia, es obligatorio esto:
+        super(patas);
+    }
+}
+
+class Reptil extends Animal {
+    constructor(patas) {
+        // cuando se hace la herencia, es obligatorio esto:
+        super(patas);
+    }
+}
+
+class Ave extends Animal {
+    constructor(patas) {
+        // cuando se hace la herencia, es obligatorio esto:
+        super(patas);
+    }
+}
--- creando_a_groot.js
+++ creando_a_groot.js
@@ -7,6 +7,7 @@ class Groot {
         // atributo o propiedad privada
         this.#fechaDeAparicion = fechaDeAparicion;
     }
+    succionarMadera() {}
 }
 
 // crear la instancia
