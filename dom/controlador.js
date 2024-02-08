4b4ed9794c8cd0ac5aa485cbad9b32568c2aea7a
gallopeladoprofe <juanftp100.edu@gmail.com>
2024-02-08 14:20:54 -0300

    readme agreado

--- dom/controlador.js
+++ dom/controlador.js
@@ -1,22 +1,52 @@
 
 // creando nodos de nuestra pagina
+//Campos de texto
 const txtnombres = document.getElementById("txtNombres");
 const txtapellidos  = document.getElementById("txtApellidos");
-const Rsexo = document.getElementsByName("sexo");
+
+//Array de objetos del radiobutton
+//Presta atención, usamos el getElementsByName
+const arrayRadioSexo = document.getElementsByName("sexo");
+
+//Botones
 const btnCrear = document.getElementById("btnCrear");
 const btnSalir = document.getElementById("btnSalir");
 
-btnCrear.addEventListener("click", function(){
-    // esto recupera el valor de nombre
+//Se le asigna el evento click al botón crear
+btnCrear.addEventListener("click", function() {
+
+    //esto recupera el valor de nombre
     const nombres = txtnombres.value.trim();
     //valido el nombre
     if(!nombres) {
-        alert("Capelus, te falta el nombre");
+        alert("Falta el nombre");
+        return;
+    }
+    //esto recupera el valor de apellidos
+    const apellidos = txtapellidos.value.trim();
+    if(!apellidos) {
+        alert("Falta apellidos");
+        return;
     }
 
     // recorrer el array de sexo
-    for(let item of Rsexo) {
-        console.log(item.checked);
+    let estaMarcadoElSexo = false;
+    for(let item of arrayRadioSexo) {
+        //Revisar si la propiedad checked es true
+        //Revisar si el value del item para saber si es M o F
+        if(item.checked && item.value === "M") {
+            estaMarcadoElSexo = true;
+            console.log(item.checked);
+        }
+        if(item.checked && item.value === "F") {
+            estaMarcadoElSexo = true;
+            console.log(item.checked);
+        }
+    }
+    //Si no se marcó ningun radiobutton se avisa al usuario
+    if(!estaMarcadoElSexo) {
+        alert("¿Debe seleccionar el sexo!");
+        return;
     }
 
 });
---
+++ dom/readme.md
@@ -0,0 +1,11 @@
+### Ejemplo usando Javascript nativo para practicar
+#### Objetivos
+- Empezar a usar el Document Object Model (DOM) para definir elementos de las páginas web.
+- Comprender el uso semántico de las etiquetas HTML para usarlo correctamente en la escritura de páginas web.
+- Comprender el uso de atributos de las etiquetas HTML para aprovechar el beneficio y personalización.
+- Aplicar el conocimiento de Javascript básico para enlazar código a un fichero.
+- Comprender el uso de `getElementById()` para aplicar en elementos HTML.
+- Comprender el uso de `getElementsByName()` para obtener nodos que tengan el atributo `name`.
+- Comprender la creación de eventos con `addEventListener()` para usar con otras etiquetas HTML.
+- Aplicar el concepto de función anónima dentro de `addEventListener()` para ejecutar código en el evento click.
+- Aplicar el concepto del bucle `for of` para recorrer los radiobuttons
