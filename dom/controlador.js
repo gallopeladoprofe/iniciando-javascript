8ff36ba6c3174600d985023402f7fc2da46efc58
gallopeladoprofe <juanftp100.edu@gmail.com>
2024-02-12 18:56:42 -0300

    add: usar localstorage para guardar un usuario

--- dom/controlador.js
+++ dom/controlador.js
@@ -1,20 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    //zona segura
+    const btnCrear = document.getElementById("btnCrear");
+    const btnSalir = document.getElementById("btnSalir");
+    
+    //Se le asigna el evento click al botón crear
+    btnCrear.addEventListener("click", function() {
+        capturarDatosDelFormulario();
+    });
+});
 
-// creando nodos de nuestra pagina
-//Campos de texto
-const txtnombres = document.getElementById("txtNombres");
-const txtapellidos  = document.getElementById("txtApellidos");
+function capturarDatosDelFormulario() {
+    // creando nodos de nuestra pagina
+    //Campos de texto
+    const txtnombres = document.getElementById("txtNombres");
+    const txtapellidos  = document.getElementById("txtApellidos");
 
-//Array de objetos del radiobutton
-//Presta atención, usamos el getElementsByName
-const arrayRadioSexo = document.getElementsByName("sexo");
-
-//Botones
-const btnCrear = document.getElementById("btnCrear");
-const btnSalir = document.getElementById("btnSalir");
-
-//Se le asigna el evento click al botón crear
-btnCrear.addEventListener("click", function() {
-
+    //Array de objetos del radiobutton
+    //Presta atención, usamos el getElementsByName
+    const arrayRadioSexo = document.getElementsByName("sexo");
     //esto recupera el valor de nombre
     const nombres = txtnombres.value.trim();
     //valido el nombre
@@ -29,6 +32,8 @@ btnCrear.addEventListener("click", function() {
         return;
     }
 
+    let femenino = null;
+    let masculino = null;
     // recorrer el array de sexo
     let estaMarcadoElSexo = false;
     for(let item of arrayRadioSexo) {
@@ -36,17 +41,31 @@ btnCrear.addEventListener("click", function() {
         //Revisar si el value del item para saber si es M o F
         if(item.checked && item.value === "M") {
             estaMarcadoElSexo = true;
+            masculino = item.value;
             console.log(item.checked);
         }
         if(item.checked && item.value === "F") {
             estaMarcadoElSexo = true;
+            femenino = item.value;
             console.log(item.checked);
         }
     }
     //Si no se marcó ningun radiobutton se avisa al usuario
     if(!estaMarcadoElSexo) {
-        alert("¿Debe seleccionar el sexo!");
+        alert("Debe seleccionar el sexo!");
         return;
     }
 
-});
+    // si llegamos hasta aqui, quiere decir que esta bien validado 
+    const usuario = {
+        nombres: nombres
+        , apellidos: apellidos
+        , sexo: masculino ? masculino : femenino
+    };
+    console.log(usuario);
+
+    //Proceso de guardado
+    const crearJsonUsuario = JSON.stringify(usuario);
+    console.log(crearJsonUsuario);
+    localStorage.setItem("usuario", crearJsonUsuario);
+}
